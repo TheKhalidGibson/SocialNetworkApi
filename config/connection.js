b6c68a863b31a519e1d3db8246605ff1fc82eabe
Khalid G <kgibson4795@gmail.com>
2023-09-17 00:20:45 -0400

    changed some of the syntax in my connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,17 @@
 const { connect, connection } = require('mongoose');
 
-const connectionString =
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialDB';
-
-connect(connectionString);
+connect('mongodb://127.0.0.1:27017/socialDB');
 
 module.exports = connection;
+
+
+
+
+// const { connect, connection } = require('mongoose');
+
+// const connectionString =
+//   process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialDB';
+
+// connect(connectionString);
+
+// module.exports = connection;
