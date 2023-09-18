4c53efc0b4a6519aeddc3edc166cfb580efc2130
Khalid G <kgibson4795@gmail.com>
2023-09-18 16:08:17 -0400

    added update, add and remove  users friend

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -31,4 +31,87 @@ module.exports = {
       res.status(500).json(err);
     }
   },
-};
+  async updateUser(req, res) {
+  try {
+    const user = await User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    );
+
+    if (!user) {
+      return res.status(404).json({ message: 'No user with this id!' });
+    }
+    
+    res.json(user);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+},
+
+async deleteUser(req, res) {
+  try {
+    const userOne = await User.findOneAndRemove({ _id: req.params.userId });
+
+    if (!userOne) {
+      return res.status(404).json({ message: 'No user with this id!' });
+    }
+
+    const user = await User.findOneAndUpdate(
+      { users: req.params.userId },
+      { $pull: { users: req.params.userId } },
+      { new: true }
+    );
+
+    if (!user) {
+      return res
+        .status(404)
+        .json({ message: 'User deleted but no user with this id!' });
+    }
+
+    res.json({ message: 'User successfully deleted!' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+},
+
+async addUserFriend(req, res) {
+  try {
+    const user = await User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $addToSet: { friends: req.params.friendId } },
+      { runValidators: true, new: true }
+    );
+
+    if (!user) {
+      return res.status(404).json({ message: 'No user with this id!' });
+    }
+
+    res.json(user);
+  } catch (err) {
+    console.log(err)
+    res.status(500).json(err);
+  }
+},
+// Remove user friend
+async removeUserFriend(req, res) {
+  try {
+    const user = await User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $pull: { friends: req.params.friendId } },
+      { runValidators: true, new: true }
+    )
+
+    if (!user) {
+      return res.status(404).json({ message: 'No user with this id!' });
+    }
+
+    res.json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+}
+
+
+}
