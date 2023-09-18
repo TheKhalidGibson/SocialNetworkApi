0c987054cd9545fd5c0c68a31192aef41bf01f5d
Khalid G <kgibson4795@gmail.com>
2023-09-18 16:09:25 -0400

    commented out unneeded code

---
+++ seeds/index.js
@@ -0,0 +1,22 @@
+// const seedUsers = require('./userData');
+// const seedThoughts = require('./thoughtData');
+
+
+// const sequelize = require('../config/connection');
+
+// const seedAll = async () => {
+//   await sequelize.sync({ force: true });
+//   console.log('\n----- DATABASE SYNCED -----\n');
+ 
+//   await seedUsers();
+//   console.log('\n----- USERS SEEDED -----\n');
+
+//   await seedThoughts();
+//   console.log('\n----- THOUGHTS SEEDED -----\n');
+
+
+
+//   process.exit(0);
+// };
+
+// seedAll();
