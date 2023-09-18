97ff3bfce3831116a6cba0c0574dee8184b23e54
Khalid G <kgibson4795@gmail.com>
2023-09-18 16:08:58 -0400

    allowed all of the routes to be useable

--- Routes/api/userRoutes.js
+++ Routes/api/userRoutes.js
@@ -3,12 +3,27 @@ const {
   getUsers,
   getSingleUser,
   createUser,
+  updateUser,
+  deleteUser,
+  addUserFriend,
+  removeUserFriend,
 } = require('../../controllers/userControllers');
 
 // /api/users
 router.route('/').get(getUsers).post(createUser);
 
 // /api/users/:userId
-router.route('/:userId').get(getSingleUser);
+router.route('/:userId')
+.get(getSingleUser)
+.put(updateUser)
+.delete(deleteUser);
+
+
+
+router.route('/:userId/friends/:friendId')
+.delete(removeUserFriend)
+.post(addUserFriend);
+
+
 
 module.exports = router;
